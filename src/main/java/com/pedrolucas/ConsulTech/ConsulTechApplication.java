a842d296eb6bd3e7b285361e2ee7cdfc48d13dce
Pedro Lucas <plpvalgas@gmail.com>
2025-09-12 08:27:47 -0300

    Criado estrutura inicial do projeto

---
+++ src/main/java/com/pedrolucas/ConsulTech/ConsulTechApplication.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.ConsulTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrolucas/ConsulTech/ConsulTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolucas.ConsulTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
